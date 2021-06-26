fca1258c42718c22d4e326f91b7f237de78595f7
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-26 19:39:24 -0300

    Referenciando os posts do usuário

--- src/main/java/com/netomarcelino/workshopmongo/config/Instantiation.java
+++ src/main/java/com/netomarcelino/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.netomarcelino.workshopmongo.config;
 
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -42,6 +43,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria); 
 	}
 
 }
