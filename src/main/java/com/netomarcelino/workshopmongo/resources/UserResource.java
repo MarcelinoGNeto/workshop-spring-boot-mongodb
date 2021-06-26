a335d9ccfba7e6ae480bb701d44e2fa0bbeace1e
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-26 11:25:19 -0300

    Atualizacao de usuario com o PUT

--- src/main/java/com/netomarcelino/workshopmongo/resources/UserResource.java
+++ src/main/java/com/netomarcelino/workshopmongo/resources/UserResource.java
@@ -30,12 +30,14 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+//--------------------------------------C R U D ------------------------------------------
 
-//Método para pesquisar dados no banco
+	//Método para pesquisar dados no banco
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-		return ResponseEntity.ok().body(new UserDTO(obj));//ok: código 404
+		return ResponseEntity.ok().body(new UserDTO(obj));// ok: código 404
 	}
 
 //Método para inserir dados no banco
@@ -44,14 +46,22 @@ public class UserResource {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build(); //created: código 201
+		return ResponseEntity.created(uri).build(); // created: código 201
 	}
 
 	// Método para deletar dados no banco
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
-		return ResponseEntity.noContent().build();//noContent: código 204
+		return ResponseEntity.noContent().build();// noContent: código 204
 	}
 
+	// Método para atualizar dados no banco
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id ) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build(); // noContent: código 204
+	}
 }
