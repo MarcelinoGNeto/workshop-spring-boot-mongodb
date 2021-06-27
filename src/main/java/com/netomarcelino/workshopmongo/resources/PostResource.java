1bfdcd69070481963ce7797f25e26f7f338a8d6c
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-26 22:49:21 -0300

    Consulta com vários critérios - fim do curso

--- src/main/java/com/netomarcelino/workshopmongo/resources/PostResource.java
+++ src/main/java/com/netomarcelino/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.netomarcelino.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,4 +37,16 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);// ok: código 404
 	}
 
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
