a75368c2935586ae935dfbfd9ef35b3305d73167
Marcelino Neto <marcelino.gneto@gmail.com>
2021-06-26 21:09:21 -0300

    Consulta simples com query methods

--- src/main/java/com/netomarcelino/workshopmongo/resources/PostResource.java
+++ src/main/java/com/netomarcelino/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.netomarcelino.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.netomarcelino.workshopmongo.domain.Post;
+import com.netomarcelino.workshopmongo.resources.util.URL;
 import com.netomarcelino.workshopmongo.services.PostService;
 
 @RestController
@@ -22,6 +26,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);// ok: código 404
+	}
+
+	//Método para pesquisar qualquer valor no banco
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);// ok: código 404
 	}
 
 }
